8f7f7d14b08b07c26d6361a839b37fb84bbc437c
Ryan Gadd <ryangadd@Ryans-MacBook-Pro.local>
2017-03-21 23:09:09 -0500

    Homework Due

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,17 @@
-// Variables
- 
-var playerTotal = 0;
-var wins = 0;
-var losses = 0;
-var randomTargetNumber = 0;
-
 //
-
 $(document).ready(function() {
-	
-	$("#randomNumberButton").on("click", function() {
-    	var randomTargetNumber = Math.floor(Math.random() * 121-19) + 19;
-        $("#randomNumberSection").html("The target number is: " +randomTargetNumber);
-        return randomTargetNumber;
-    });
 
-    console.log(randomTargetNumber);
+     var playerTotal = 0;
+     var wins = 0;
+     var losses = 0;
+     var randomTargetNumber = 0;
+
+	$("#randomNumberButton").on("click", function() {
+    	randomTargetNumber = Math.floor(Math.random() * 120) + 19;
+        $("#randomNumberSection").html("The target number is: " + randomTargetNumber);
+        console.log(randomTargetNumber);
+     });
+
 
 	function randomGemNumber(){
 		var gemNumber = Math.floor(Math.random() * 12) + 1;
@@ -30,59 +26,23 @@ $(document).ready(function() {
     $(".gems").on("click", function() {
     	playerTotal = playerTotal + parseInt($(this).attr("value"));
     	$(".playerScore").html(playerTotal);
-    
-
+     	checkStatusOfGame();
     });
 
-    // if (playerTotal === randomTargetNumber) {
-    // 	$("#announcement").html(" You've Win!")
-    // }
+    // Make a function run whenever something is clicked.  Make sure to put this function inside the document.ready().
+     function checkStatusOfGame() {
+          console.log(playerTotal);
+          if (playerTotal === randomTargetNumber) {
+          	$("#announcement").html(" You've won!");
+          	alert("You've won! Refresh the browser to play again.")
+            }
+          else if (playerTotal > randomTargetNumber){
+          	$("#announcement").html("You've lost.");
+          	alert("You've lost. Please refresh the browser to play again.");
+          }
+        };
 
-    // else if (playerTotal > randomTargetNumber) {
-    // 		$("#announcement").html(" You've lost!")
-    // 	};
-    
- 
 
+            
+  
 });
-
-
-///When one of the crystals gets clicked
-// $(".crystal").on("click", function(){
-
-//     //Add the value attribute of the clicked crystal to the player total
-//     playerTotal += parseInt($(this).attr("value"));
-//     //Update the player's score in the html
-//     $(".playerScore").html("Your total score is: " + playerTotal);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
